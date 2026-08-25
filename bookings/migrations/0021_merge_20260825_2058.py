505c7e91a9598288b3b57067e59e85b9eeb59a6f
Serka <serkafox@gmail.com>
2026-08-25 20:57:24 +0200

    Merge booking migration branches

---
+++ bookings/migrations/0021_merge_20260825_2058.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("bookings", "0020_add_booking_group_id"),
+        ("bookings", "0020_alter_booking_extra_duration_minutes"),
+    ]
+
+    operations = []
